ba2fd52db197b2b00c1fad40e52204fd1782ddef
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-22 10:35:13 +0700

    Add script file and code examples for array map operation which takes values of an array, iterat es through each index like we can do with a for loop and passes new values to a new array with our assigned operation. Note the placing of the enclosing map parentheses. We are also using an inline function with parameters - the first parameter is the arrays value and the second parameter is the arrays index position.

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// the trad way of passing new values to empty array through a for loop
+const fahrenheit = [80, 72, 75,79,69];
+const celcius = [];
+for (const element of fahrenheit) {
+    let c = (element - 32) * (5/9);
+    celcius.push(c);
+}
+console.log(celcius)
+
+const value = [32, 25, 53, 74];
+const evenOrOdd = value.map(function(element) {
+    return element % 2 === 0;
+});
+// because of the inline function notice the placement of enclosing map parameters
+console.log(evenOrOdd)
+
+const queue  = ["Jack", "Jonas", "Jeff"];
+const displayQueue = queue.map(function(element, index) {
+return index + " " + element;
+});
+console.log(displayQueue);
+
+
+
+
+
+
